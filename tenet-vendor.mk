809009d5e990fd279aa9508ab5484dff410692ce
Alexander Martinz <amartinz@shiftphones.com>
2023-01-09 17:25:20 +0100

    tenet: drop aptx blobs
    They live within the bluetooth apex now.
    
    Change-Id: I01ef1d665ce05b7ce5b1ee03129ccf3ce6058634
    Signed-off-by: Alexander Martinz <amartinz@shiftphones.com>

--- tenet-vendor.mk
+++ tenet-vendor.mk
@@ -50,8 +50,6 @@ PRODUCT_COPY_FILES += \
     vendor/zte/tenet/proprietary/system_ext/lib64/lib-imsvt.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/lib-imsvt.so \
     vendor/zte/tenet/proprietary/system_ext/lib64/lib-imsvtextutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/lib-imsvtextutils.so \
     vendor/zte/tenet/proprietary/system_ext/lib64/lib-imsvtutils.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/lib-imsvtutils.so \
-    vendor/zte/tenet/proprietary/system_ext/lib64/libaptXHD_encoder.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libaptXHD_encoder.so \
-    vendor/zte/tenet/proprietary/system_ext/lib64/libaptX_encoder.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libaptX_encoder.so \
     vendor/zte/tenet/proprietary/system_ext/lib64/libdiag_system.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libdiag_system.so \
     vendor/zte/tenet/proprietary/system_ext/lib64/libdpmctmgr.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libdpmctmgr.so \
     vendor/zte/tenet/proprietary/system_ext/lib64/libdpmfdmgr.so:$(TARGET_COPY_OUT_SYSTEM_EXT)/lib64/libdpmfdmgr.so \
