14d3626ea728ef00941e4000d624f22125981568
Bruno Martins <bgcngm@gmail.com>
2024-06-23 15:42:45 +0100

    tenet: Declare IMS libs as symlinks during extraction
    Change-Id: Ia1b8097d28dd4888232fe383b830515b1b9bafaa

--- tenet-vendor.mk
+++ tenet-vendor.mk
@@ -1282,3 +1282,7 @@ PRODUCT_PACKAGES += \
     c2_manifest_vendor.xml \
     manifest_android.hardware.drm@1.3-service.widevine.xml \
     vendor.qti.gnss@4.0-service.xml
+
+PRODUCT_PACKAGES += \
+    libimscamera_jni_libimscamera_jni_symlink64 \
+    libimsmedia_jni_libimsmedia_jni_symlink64
