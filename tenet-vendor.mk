fbb16dc4dc74ef247609e42b3a54ee05e388f458
Cosmin Tanislav <demonsingur@gmail.com>
2024-08-17 02:03:24 +0300

    tenet: fix vendor.qti.imsrtpservice@3.0 ELF checks
    Change-Id: Ia1995d0a3360317843685082e3226ab30f124d09

--- tenet-vendor.mk
+++ tenet-vendor.mk
@@ -986,7 +986,7 @@ PRODUCT_PACKAGES += \
     vendor.qti.ims.rcsconfig@2.0 \
     vendor.qti.ims.rcsconfig@2.1 \
     vendor.qti.imsrtpservice@3.0-service-Impl \
-    vendor.qti.imsrtpservice@3.0 \
+    vendor.qti.imsrtpservice@3.0-vendor \
     vendor.qti.latency@2.0 \
     vendor.qti.qspmhal@1.0-impl \
     vendor.qti.qspmhal@1.0 \
