5160e882b16a247e809a2fa0efe9d7e6e2dc7134
anrui2032 <anrui2032@gmail.com>
2025-10-11 21:41:31 +0800

    tenet: Hex-edit fingerprint.gf95xx.so to set soname
    Change-Id: Ie08319001f8171126729022c2e3071b572200ca4

--- tenet-vendor.mk
+++ tenet-vendor.mk
@@ -812,7 +812,7 @@ PRODUCT_COPY_FILES += \
     vendor/zte/tenet/proprietary/vendor/lib64/hw/camera.qcom.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/camera.qcom.so \
     vendor/zte/tenet/proprietary/vendor/lib64/hw/com.qti.chi.override.bitra.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/com.qti.chi.override.bitra.so \
     vendor/zte/tenet/proprietary/vendor/lib64/hw/com.qti.chi.override.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/com.qti.chi.override.so \
-    vendor/zte/tenet/proprietary/vendor/lib64/hw/fingerprint.gf95xx.default.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.gf95xx.default.so \
+    vendor/zte/tenet/proprietary/vendor/lib64/hw/fingerprint.gf95xx.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/fingerprint.gf95xx.so \
     vendor/zte/tenet/proprietary/vendor/lib64/hw/vendor.qti.esepowermanager@1.1-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.qti.esepowermanager@1.1-impl.so \
     vendor/zte/tenet/proprietary/vendor/lib64/hw/vendor.qti.gnss@4.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.qti.gnss@4.0-impl.so \
     vendor/zte/tenet/proprietary/vendor/lib64/hw/vendor.qti.hardware.alarm@1.0-impl.so:$(TARGET_COPY_OUT_VENDOR)/lib64/hw/vendor.qti.hardware.alarm@1.0-impl.so \
