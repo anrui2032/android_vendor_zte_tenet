c7c6e9518474ef8cf65e9f7caba006bddf732f0e
anrui2032 <anrui2032@gmail.com>
2025-10-26 15:00:56 +0800

    tenet: Regenerate makefiles
    Change-Id: I4aa8e667d4b2c21d6610a8cc6e48bce98ad181e2

--- tenet-vendor.mk
+++ tenet-vendor.mk
@@ -1254,9 +1254,9 @@ PRODUCT_PACKAGES += \
     dpmapi \
     qcrilhook \
     vendor.qti.hardware.alarm-V1.0-java \
-    android.hardware.gnss@2.1-service-qti \
-    android.hardware.neuralnetworks@1.3-service-qti-hta \
-    android.hardware.neuralnetworks@1.3-service-qti \
-    c2_manifest_vendor \
-    manifest_android.hardware.drm@1.3-service.widevine \
-    vendor.qti.gnss@4.0-service
+    android.hardware.gnss@2.1-service-qti.xml \
+    android.hardware.neuralnetworks@1.3-service-qti-hta.xml \
+    android.hardware.neuralnetworks@1.3-service-qti.xml \
+    c2_manifest_vendor.xml \
+    manifest_android.hardware.drm@1.3-service.widevine.xml \
+    vendor.qti.gnss@4.0-service.xml
